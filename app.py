0fe8c05d9a93580ab083d11d6cea8a7588f06e29
AnyelRosas <luisangelrosasmeza@gmail.com>
2024-07-17 12:00:15 -0700

    Anyel
    Init

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from pytrends.request import TrendReq
+
+app = Flask(__name__)
+
+@app.route('/google-trends', methods=['POST'])
+def google_trends():
+    data = request.json
+    palabras_clave = data['palabras_clave']
+    pytrends = TrendReq(hl='es-MX', tz=360)
+    pytrends.build_payload(palabras_clave, cat=0, timeframe='today 12-m', geo='MX', gprop='')
+    tendencias = pytrends.interest_over_time()
+    if not tendencias.empty:
+        result = tendencias.reset_index().to_dict(orient='records')
+    else:
+        result = []
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
